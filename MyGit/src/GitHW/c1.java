82071190b593e8e9a4d52a39a12e35c563fa67e1
0805kkm <kkm0805@skuniv.ac.kr>
2023-12-23 11:49:24 +0900

    Merge branch 'dev1'

--- MyGit/src/GitHW/c1.java
+++ MyGit/src/GitHW/c1.java
@@ -5,7 +5,7 @@ public class c1 {
 	public static void main(String[] args) {
 		System.out.println("Hello1");
 		System.out.println("this is 2nd c1");
-		System.out.println("dev1");
+		System.out.println("dev1"); 
 	}
 
 }
